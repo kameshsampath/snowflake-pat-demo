a9f050a0be7f6af99262acabfbac5c1d444fa98f
Kamesh Sampath <kamesh.sampath@hotmail.com>
2025-05-24 09:57:55 +0530

    Rename network policy
    Rename network policy to be more apt and suited for the demo use case

--- create_network_rule.sh
+++ create_network_rule.sh
@@ -14,18 +14,18 @@ CIDR_VALUE_LIST="${GH_CIDRS},'${LOCAL_IP}'"
 snow sql --stdin <<EOF
 use role $MY_PAT_DEMO_ROLE;
 -- create a new network rule that allows all CIDR (for demo only)
-create network rule if not exists my_demo_db.networks.pat_allow_all_access_rule
+create network rule if not exists my_demo_db.networks.pat_gh_actions_local_access_rule
 mode = ingress
 type = ipv4
 value_list = ($CIDR_VALUE_LIST)
-comment = 'Allow all IPv4 addresses for demo purposes.';
+comment = 'Allow only GitHub Actions and local machine IPv4 addresses';
 
 use role accountadmin;
 -- attach the network rule to the network policy
-create network policy if not exists ALLOW_ALL_PAT_NETWORK_POLICY
-allowed_network_rule_list = ('my_demo_db.networks.pat_allow_all_access_rule')
+create network policy if not exists GH_ACTIONS_PAT_NETWORK_POLICY
+allowed_network_rule_list = ('my_demo_db.networks.at_gh_actions_local_access_rule')
 comment = 'Network policy to allow all IPv4 addresses.';
 
 -- set the network policy to the user to allow using pat with snowflake cli
-alter user $SNOWFLAKE_USER set network_policy='ALLOW_ALL_PAT_NETWORK_POLICY';
+alter user $SNOWFLAKE_USER set network_policy='GH_ACTIONS_PAT_NETWORK_POLICY';
 EOF
